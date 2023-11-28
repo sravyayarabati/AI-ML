3e1a06174b4b5db3d0eb2a10aa17caabbc6d7804
sravyayarabati <139531728+sravyayarabati@users.noreply.github.com>
2023-11-28 11:11:41 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,9 @@
+print('First Program in ML Space!')
+
+#print('First Program in ML Space!')
+#print('First Program in ML Space!')
+
+def Helloworld():
+    """This is a function"""
+
+    Helloworld
